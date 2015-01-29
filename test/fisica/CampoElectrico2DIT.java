0e87a8df67633afa22bbc6791945a66f35d38596
miguel550 <miguel.almonte1@gmail.com>
2015-01-29 08:39:06 -0400

    commit to make travis work

--- test/fisica/CampoElectrico2DIT.java
+++ test/fisica/CampoElectrico2DIT.java
@@ -159,7 +159,6 @@ public class CampoElectrico2DIT {
         String result = instance.toString();
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
     
 }
